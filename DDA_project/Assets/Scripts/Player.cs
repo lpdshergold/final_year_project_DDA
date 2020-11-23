adbf9b148ddf450869c20b819aa3a45611c57ecf
lpdshergold <L.Shergold1@uni.brighton.ac.uk>
2020-11-23 00:53:58 +0000

    Added shoot functionality
    Created a script that will allow the player to shoot in the direction of the gun

--- DDA_project/Assets/Scripts/Player.cs
+++ DDA_project/Assets/Scripts/Player.cs
@@ -1,25 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private int playerSpeed = 2;
-    [SerializeField] private GameObject weapon;
     [SerializeField] private Sprite choosenGun;
+    [SerializeField] private GameObject weapon;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform bulletSpawn;
 
     private Transform weaponAim;
     private Rigidbody2D rb;
-    private SpriteRenderer ren, gun;
+    private SpriteRenderer playerFlip, gun;
 
     private bool fX = false;
+    private bool isFiring = false;
+
+    [SerializeField] float delayShoot = .25f;
 
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        ren = GetComponent<SpriteRenderer>();
+        playerFlip = GetComponent<SpriteRenderer>();
         gun = weapon.GetComponent<SpriteRenderer>();
         weaponAim = weapon.transform;
         gun.sprite = choosenGun;
@@ -33,8 +37,10 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         PlayerMovement();
+        ShootGun();
     }
 
+    // Moves the player character
     private void PlayerMovement() {
         if(Input.GetKey(KeyCode.D)) {
             rb.velocity = new Vector2(playerSpeed, 0);
@@ -50,28 +56,58 @@ public class Player : MonoBehaviour
             rb.velocity = new Vector2(0, 0);
         }
 
-        ren.flipX = fX;
+        playerFlip.flipX = fX;
     }
 
     private void RotateGun() {
+        // get the mouse position
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+        // get the aim direction for the gun
         Vector3 aimDir = (mousePos - weaponAim.transform.position);
 
+        // rotation of the mouse
         float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg;
 
-        Vector3 tempVector = weaponAim.transform.localScale;
+        // Used to flip the weapon scale
+        Vector3 tempScaleVector = weaponAim.transform.localScale;
 
         if(fX) {
             angle = Mathf.Atan2(-aimDir.y, -aimDir.x) * Mathf.Rad2Deg;
-            tempVector.x = -1;
-            weaponAim.transform.localScale = tempVector;
+            
+            // flip the weapon
+            tempScaleVector.x = -1;
+            weaponAim.transform.localScale = tempScaleVector;
         } else {
             angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg;
-            tempVector.x = 1;
-            weaponAim.transform.localScale = tempVector;
+
+            // flip the weapon
+            tempScaleVector.x = 1;
+            weaponAim.transform.localScale = tempScaleVector;
         }
 
         weaponAim.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+
+    // MOVE SHOOTGUN AND RESETSHOOT TO THEIR OWN SCRIPT THAT WILL BE PUT ON THE BULLET PREFAB
+    private void ShootGun() {
+        if (Input.GetKey(KeyCode.Mouse0)) {
+            if(isFiring) { return; }
+
+            isFiring = true;
+            GameObject b = Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+            if (fX) {
+                b.GetComponent<Rigidbody2D>().velocity = bulletSpawn.right * -5f; 
+            } else {
+                b.GetComponent<Rigidbody2D>().velocity = bulletSpawn.right * 5f;
+            }
+            Destroy(b, 5f);
+            Invoke("ResetShoot", delayShoot);
+        }
+    }
+
+    private void ResetShoot() {
+        isFiring = false;
     }
 }
